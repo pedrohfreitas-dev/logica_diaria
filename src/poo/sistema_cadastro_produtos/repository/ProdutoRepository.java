85883fa499562e19596775f9005e8d4b0d866128
Pedro Freitas <pedrofreitaskt@gmail.com>
2026-08-17 15:32:37 -0300

    feat: Método listarProdutos criado

--- src/poo/sistema_cadastro_produtos/repository/ProdutoRepository.java
+++ src/poo/sistema_cadastro_produtos/repository/ProdutoRepository.java
@@ -1,8 +1,11 @@
 package poo.sistema_cadastro_produtos.repository;
 
 import poo.sistema_cadastro_produtos.model.Produto;
+import poo.sistema_cadastro_produtos.service.ProdutoService;
 
+import javax.swing.plaf.ProgressBarUI;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProdutoRepository {
@@ -11,12 +14,20 @@ public class ProdutoRepository {
 
     int contador = 0;
 
-    public Produto salvar(Produto produto) {
+    public int salvar(Produto produto) {
         listaProdutos.add(produto);
         int idproduto = ++contador;
         produto.setId(idproduto);
-        return listaProdutos.getLast();
+        return produto.getId();
     }
+
+    public List<Produto> listarTodos() {
+      return listaProdutos;
+    }
+
+
+
+
 
 
 }
