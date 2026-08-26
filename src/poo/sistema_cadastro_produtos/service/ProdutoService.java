55368d82d5b8f1b766c469079766bde37eca2424
Pedro Freitas <pedrofreitaskt@gmail.com>
2026-08-26 18:50:23 -0300

    feat: método cadastrar criado

---
+++ src/poo/sistema_cadastro_produtos/service/ProdutoService.java
@@ -0,0 +1,26 @@
+package poo.sistema_cadastro_produtos.service;
+
+import poo.sistema_cadastro_produtos.dto.ProdutoDTO;
+import poo.sistema_cadastro_produtos.model.Produto;
+import poo.sistema_cadastro_produtos.repository.ProdutoRepository;
+import poo.sistema_conta_bancaria.exceptions.ValorInvalidoException;
+
+public class ProdutoService {
+
+    private ProdutoRepository repository;
+
+    public ProdutoService(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public Produto cadastrar(ProdutoDTO dto) {
+        if (dto.getPreco() > 0) {
+            Produto produto  = new Produto(dto.getPreco(),dto.getNome());
+            Produto produtoSalvo = repository.salvar(produto);
+            return produtoSalvo;
+        }
+        throw new ValorInvalidoException("Valor Inválido");
+    }
+
+}
