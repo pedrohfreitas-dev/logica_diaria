54613e6a1a501a8879d0734fb5a3ee9385d0ffd4
Pedro Freitas <pedrofreitaskt@gmail.com>
2026-08-26 17:37:14 -0300

    feat: Exception ProdutoNaoEncontradoException criada

---
+++ src/poo/sistema_cadastro_produtos/exceptions/ProdutoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package poo.sistema_cadastro_produtos.exceptions;
+
+public class ProdutoNaoEncontradoException extends RuntimeException {
+
+    public ProdutoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
